6e835367ad586249f7c6f909d98ec5a62929885c
Jon Aumann <jaumann@gmail.com>
2020-05-21 13:03:03 -0400

    Add logic for dryrun

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
 # Configuration
-default_semvar_bump=${DEFAULT_BUMP:-minor}
+default_semvar_bump=${DEFAULT_BUMP:-patch}
 source=${SOURCE:-.}
-dryrun=${DRY_RUN:-false}
+dryrun=${DRY_RUN:-true}
 
-cd ${GITHUB_WORKSPACE}/${source}
+cd "${GITHUB_WORKSPACE}/${source}" || return
 
 # get latest tag that looks like a semver (with or without v)
 git fetch --tags
 tag=$(git for-each-ref --sort=-v:refname --count=1 --format '%(refname)' refs/tags/[0-9]*.[0-9]*.[0-9]* refs/tags/v[0-9]*.[0-9]*.[0-9]* | cut -d / -f 3-)
-tag_commit=$(git rev-list -n 1 $tag)
+tag_commit=$(git rev-list -n 1 "$tag")
 
 # get current commit hash for tag
 commit=$(git rev-parse HEAD)
 
 if [ "$tag_commit" == "$commit" ]; then
   echo "No new commits since previous tag. Skipping..."
-  echo ::set-output name=tag::$tag
+  echo ::set-output name=tag::"$tag"
   exit 0
 fi
 
@@ -30,27 +30,34 @@ if [ -z "$tag" ]
 then
   log=$(git log --pretty='%B')
 else
-  log=$(git log $tag..HEAD --pretty='%B')
+  log=$(git log "$tag"..HEAD --pretty='%B')
 fi
 
 case "$log" in
   *#major* ) part="major";;
   *#minor* ) part="minor";;
   *#patch* ) part="patch";;
-  * ) part=$default_semvar_bump;;
+  * ) part="$default_semvar_bump";;
 esac
 
-raw_output=$(bumpversion --list $part --dry-run --allow-dirty)
-old_version=$(echo $raw_output | grep -o 'current_version=\S*' | cut -d= -f2)
-new_version=$(echo $raw_output | grep -o 'new_version=\S*' | cut -d= -f2)
+raw_output=$(bumpversion --list "$part" --dry-run)
+old_version=$(echo "$raw_output" | grep -o 'current_version=\S*' | cut -d= -f2)
+new_version=$(echo "$raw_output" | grep -o 'new_version=\S*' | cut -d= -f2)
 
 # We should have all the information we need by this point
 # Set Outputs and log information
 
-echo ::set-output name=old_ver::$old_version
-echo ::set-output name=new_ver::$new_version
-echo ::set-output name=part::$part
+echo ::set-output name=old_ver::"$old_version"
+echo ::set-output name=new_ver::"$new_version"
+echo ::set-output name=part::"$part"
 
 echo "Semantic Version Part to Bump: $part"
 echo "Current Version: $old_version"
 echo "New Version: $new_version"
+
+if [ "$dryrun" = true ]; then
+  echo "Dryrun is set to true. Exiting..."
+  exit 0
+else
+  bumpversion "$part" --verbose
+fi
